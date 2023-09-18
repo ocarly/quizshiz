4e622cd5297aa070031aa0b0aa84f1c81b7c2652
BootCamp_Xolo <141790916+ocarly@users.noreply.github.com>
2023-09-17 21:47:05 -0500

    changed javascript and html to try to add timer

--- script.js
+++ script.js
@@ -1,112 +1,103 @@
 const questions = [
     {
-        question: "What does 'DOM' stand for in JavaScript?",
-        options: ["Document Object Model", "Data Object Model", "Database Object Model", "Document Oriented Model"],
-        correctAnswer: "0"
+        question: "What is JavaScript used for?",
+        choices: ["Styling HTML elements", "Programming web pages", "Creating databases", "Making coffee"],
+        correctAnswer: "Programming web pages"
     },
     {
-        question: "Which method is used to add new elements to an array in JavaScript?",
-        options: ["push()", "append()", "insert()", "add()"],
-        correctAnswer: "0"
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        choices: ["var", "let", "const", "variable"],
+        correctAnswer: "var"
     },
     {
-        question: "What is a closure in JavaScript?",
-        options: ["A way to lock a variable", "A private function", "A function that has access to its outer scope variables", "A function with no return value"],
-        correctAnswer: "2"
+        question: "What does 'DOM' stand for?",
+        choices: ["Document Object Model", "Data Object Model", "Document Order Model", "Dynamic Object Management"],
+        correctAnswer: "Document Object Model"
     },
-    {
-        question: "What is the purpose of 'console.log()' in JavaScript?",
-        options: ["Display a message box", "Print text to the console", "Open a new web page", "Create a pop-up window"],
-        correctAnswer: "1"
+	{
+        question: "What is the result of 10 + '5' in JavaScript?",
+        choices: ["15", "105", "1050", "Error"],
+        correctAnswer: "105"
     }
 ];
 
-let currentQuestion = 0;
-let timer = 60; // 60 seconds for the quiz
-let score = 0; 
+let currentQuestionIndex = 0;
+let timer;
+let timeLeft = 60;
+let highScore = localStorage.getItem("highScore") || Infinity;
 
-const questionElement = document.getElementById('question');
-const timerElement = document.getElementById('timer');
-const nextButton = document.getElementById('next-button');
-const inputThings = document.querySelectorAll(`input[name="answer"]`)
-const userAnswer = document.querySelector(`input[value]`)
-const endScreen = document.querySelector(".end-view")
-const inputBox = document.querySelector("#initials")
-const endNotification = document.querySelector("#end-notification")
-const userScoreEl = document.querySelector("#lil-score")
+const startButton = document.getElementById("start-btn");
+const timerDisplay = document.getElementById("timer");
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices");
+const resultElement = document.getElementById("result");
+const highScoreElement = document.getElementById("high-score");
 
+startButton.addEventListener("click", startQuiz);
 
-function displayQuestion() {
-    endScreen.style.display = 'none';
+function startQuiz() {
+    startButton.style.display = "none";
+    displayQuestion();
+    timer = setInterval(updateTimer, 1000);
+   
+}
 
-    if (currentQuestion < questions.length) {
-        questionElement.textContent = questions[currentQuestion].question;
-        const options = questions[currentQuestion].options;
+function updateTimer() {
+    timeLeft--;
 
-        const labels = document.querySelectorAll('.option-label');
-        labels.forEach((label, index) => {
-            label.textContent = options[index];
-        });
-    } else {
+    if (timeLeft <= 0) {
+        clearInterval(timer);
         endQuiz();
+    } else {
+        timerDisplay.textContent = timeLeft + " seconds";
     }
 }
 
-// Event listener for the next button
-nextButton.addEventListener('click', () => {
-    const selectedAnswer = document.querySelector('input[name="options"]:checked');
-    console.log(selectedAnswer)
-    if (selectedAnswer) {
-        const selectedValue = selectedAnswer.value;
-        console.log("selectedAnswer" , selectedValue)
-        const correctAnswer = questions[currentQuestion].correctAnswer;
-        console.log("correctAnswer" , correctAnswer)
-        if (selectedValue === correctAnswer) {
-            console.log(selectedValue, correctAnswer)
-            score+= 50;
-            console.log(score)
-        }
-
-        if (selectedValue !== correctAnswer){
-            timer -= 10;
-        }
-        
-        currentQuestion++;
-        displayQuestion();
-        
-        // Clear the radio button selection
-        selectedAnswer.checked = false;
-    } else {
-        alert("Please select an answer before proceeding.");
-    }
-    
-    // Check if the quiz has ended
-    if (currentQuestion >= questions.length) {
+function displayQuestion() {
+    if (currentQuestionIndex >= questions.length) {
         endQuiz();
+        return;
     }
-});
 
+    const currentQuestion = questions[currentQuestionIndex];
+    questionElement.textContent = currentQuestion.question;
+    choicesElement.innerHTML = "";
 
+    currentQuestion.choices.forEach((choice, index) => {
+        const choiceButton = document.createElement("button");
+        choiceButton.textContent = choice;
+        choiceButton.addEventListener("click", () => checkAnswer(choice));
+        choicesElement.appendChild(choiceButton);
+    });
+}
 
-function startTimer() {
-    const interval = setInterval(function () {
-        timer--;
-        timerElement.textContent = timer;
-        if (timer <= 0) {
-            clearInterval(interval);
-            endQuiz();
-        }
-    }, 1000);
+function checkAnswer(choice) {
+    const currentQuestion = questions[currentQuestionIndex];
+
+    if (choice === currentQuestion.correctAnswer) {
+        resultElement.textContent = "Correct!";
+    } else {
+        resultElement.textContent = "Incorrect!";
+        timeLeft -= 10;
+    }
+
+    currentQuestionIndex++;
+    setTimeout(displayQuestion, 1000);
 }
 
 function endQuiz() {
-    console.log("running?")
-    endScreen.style.display = "block"
-    endNotification.textContent = 'Quiz completed!';
-    userScoreEl.textContent = "You Scored: " + score
-    document.getElementById('question-container').style.display = 'none';
-    nextButton.style.display = 'none';
+    clearInterval(timer);
+    timerDisplay.textContent = "0 seconds";
+
+    if (timeLeft > highScore) {
+        highScore = timeLeft;
+        localStorage.setItem("highScore", highScore);
+    }
+
+    highScoreElement.textContent = highScore + " seconds";
+    questionContainer.style.display = "none";
+    resultElement.textContent = "Quiz Completed!";
 }
 
-displayQuestion();
-startTimer();
+highScoreElement.textContent = highScore + " seconds";
